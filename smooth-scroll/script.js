76cf0788f600882e4651de24c142fa7c1079f515
k.takaya <k.takaya@marukinad.co.jp>
2023-11-20 17:05:00 +0900

    スムーススクロール

--- smooth-scroll/script.js
+++ smooth-scroll/script.js
@@ -1,35 +1,42 @@
 gsap.registerPlugin(ScrollToPlugin);
 
 class SmoothScroll {
-  static init({ data = 'smooth', duration = 0.5, outAnchor = true, header = null } = {}) {
-    const initObj = new this({ data, duration, header, outAnchor });
+  static init({ duration = 0.5, ease = 'power2,inOut', header = null, outAnchor = true } = {}) {
+    const initObj = new this({ duration, ease, header, outAnchor });
     if (outAnchor) window.addEventListener('load', initObj.outAnchorLink.bind(initObj));
   }
-  constructor({ data, duration, header }) {
+  constructor({ duration, ease, header }) {
     this.header = document.querySelector(header);
     this.duration = duration;
-    this.clickEls = document.querySelectorAll(`[data-${data}]`);
+    this.ease = ease;
+    this.clickEls = document.querySelectorAll("a[href*='#']");
     this.clickEls.forEach((clickEl) => {
-      const dataset = clickEl.dataset[data];
-      if (dataset === 'anchor') {
-        clickEl.addEventListener('click', this.anchorLink.bind(this, clickEl));
-      }
+      clickEl.addEventListener('click', this.anchorLink.bind(this, clickEl));
     });
   }
-
   movingAnimation(toEl) {
-    const animationData = { duration: this.duration, scrollTo: { y: toEl, offsetY: 0 } };
+    const animationData = {
+      duration: this.duration,
+      ease: this.ease,
+      scrollTo: { y: toEl, offsetY: 0 },
+    };
     if (this.header !== null) {
       const headerHeight = this.header.offsetHeight;
       animationData.scrollTo.offsetY = headerHeight;
     }
     gsap.to(window, animationData);
   }
-
   anchorLink(clickEl, e) {
     e.preventDefault();
-    const getAttr = clickEl.getAttribute('href');
-    const toEl = document.querySelector(getAttr);
+    let attr = clickEl.getAttribute('href');
+    let toEl;
+    if (attr === '#') {
+      toEl = document.body;
+    } else {
+      attr.indexOf('/') === -1
+        ? (toEl = document.querySelector(attr))
+        : (toEl = document.querySelector(attr.substr(attr.indexOf('#'))));
+    }
     this.movingAnimation(toEl);
   }
 
